62e836d783c08d0ebecef5f6e46ce6a69c56e7a4
Sebastian <Sebastian.Liszewski@outlook.com>
2022-07-30 20:41:59 +0200

    Totolotek game - first adding.

---
+++ TotolotekCSharp/TotolotekCSharp/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotolotekCSharp
+{
+    class Program
+    {
+        static int[] LosowanieLiczb(int[] tab)
+        {
+            Random random = new Random();
+             for (int i = 0; i < 6; i++)
+             {
+                 tab[i] = random.Next(0, 49);
+             }
+            for(int i = 0; i < 6; i++) // Sprawdzanie powtórzonych liczb.
+            {
+                for(int j = 0; j < 6; j++)
+                {
+                    if((tab[i] == tab[j]) && (i != j)) // Zapobieganie powtórzeniu liczb.
+                    {
+                        tab[i] = random.Next(0, 49);
+                        i = 0;
+                        break;
+                    }
+                }
+            }
+            return tab;
+        }
+        static int[] SkreślanieLiczb(int[] tab)
+        {
+            Console.WriteLine("Proszę o skreślenie sześciu liczb od 1 do 49:");
+            Console.WriteLine();
+            for(int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Skreśl " + (i + 1) + " liczbę");
+                tab[i] = int.Parse(Console.ReadLine());
+                
+                while((tab[i] < 1) || (tab[i] > 49))
+                {
+                        Console.WriteLine("Podałeś liczbę poza zakresem, spórbuj ponownie: ");
+                        tab[i] = int.Parse(Console.ReadLine());
+                }
+            }
+            return tab;
+        }
+        static void PorównywanieLiczb()
+        {
+            int a = 0;
+            int b = 0;
+            int[] TabPom = new int[6];
+            int[] tablica1 = new int[6];
+            int[] tablica2 = new int[6];
+            LosowanieLiczb(tablica1);
+            SkreślanieLiczb(tablica2);
+            for(int i = 0; i < 6; i++)
+            {
+                for(int j = 0; j < 6; j++)
+                {
+                    if (tablica1[i] == tablica2[j])
+                    {
+                        a++;
+                        TabPom[b] = tablica1[i];
+                        b++;
+                    }
+                }
+            }
+            if (a == 0)
+            {
+                Console.WriteLine("Ups :( Nic nie trafiłeś!");
+            }
+            else if (a > 2)
+            {
+                Console.WriteLine("Gratulacje! Trafiłeś " + a + " liczb :-) - Wygrałeś pewną kwotę");
+            }
+            else
+            {
+                Console.WriteLine("Brawo! Trafiłeś " + a + " liczb ;-)");
+            }
+            Console.WriteLine();
+            if (a > 0)
+            {
+                Console.Write("Liczby, które trafiłeś, to: ");
+                for (int x = 0; x < a; x++)
+                {
+                    Console.Write(TabPom[x] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------");
+            Console.Write("Wylosowane liczby: ");
+            Console.WriteLine();
+            for(int i = 0; i < 6; i++)
+            {
+                Console.Write(tablica1[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------");
+            Console.Write("Twoje liczby: ");
+            Console.WriteLine();
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write(tablica2[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------");
+        }
+        static void Main(string[] args)
+        {
+            PorównywanieLiczb();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TotolotekCSharp/TotolotekCSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("TotolotekCSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("TotolotekCSharp")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("7aab9e4c-a324-4228-9624-e5ab84bff531")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
